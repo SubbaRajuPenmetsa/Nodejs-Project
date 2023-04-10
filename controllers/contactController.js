c279b7703ff89e9371643973fdbfcc41ac16a181
subbaraju33 <penmetsa.vinay@gmail.com>
2023-04-10 13:25:44 +0530

    Project of NodeJs and ExpressJS API

---
+++ controllers/contactController.js
@@ -0,0 +1,86 @@
+const e = require("express");
+const asyncHandler = require("express-async-handler");
+const contactModel = require("../models/contactModel");
+const Contact = require("../models/contactModel");
+//@desc Get all contacts
+//@route GET /api/contacts
+//@access private
+const getContacts = asyncHandler(async(req, res) => {
+    const contacts= await Contact.find({user_id: req.user.id});
+    res.status(200).json(contacts);
+});
+
+//@desc Create new contacts
+//@route POST /api/contacts
+//@access private
+const createContact = asyncHandler(async(req, res) => {
+    console.log("The request body is:", req.body);
+    const{name, email, phone } = req.body;
+    if(!name || !email || !phone){
+        res.status(400);
+        throw new Error("Allfields are mandatory !!!...");
+    }
+    const contact = await contactModel.create({
+        name,
+        email,
+        phone,
+        user_id: req.user.id,
+    })
+    res.status(201).json(contact);
+});
+
+//@desc get contact
+//@route GET /api/contacts/:id
+//@access private
+const getContact = asyncHandler(async(req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    res.status(200).json(contact);
+});
+
+//@desc Update contact
+//@route POST /api/contacts/:id
+//@access private
+const putContact = asyncHandler(async(req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+
+    if(contact.user_id.toString() !== req.user.id){
+        res.status(403);
+        throw new Error("user canot delete the contact created by another user")
+    }
+
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new:true}
+    );
+    res.status(200).json(updatedContact);
+});
+
+//@desc Delete contact
+//@route DELETE /api/contacts/:id
+//@access private
+const deleteContact = asyncHandler(async(req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+
+    if(contact.user_id.toString() !== req.user.id){
+        res.status(403);
+        throw new Error("user canot delete the contact created by another user")
+    }
+
+    await Contact.deleteOne({ _id: req.params.id});
+    res.status(200).json(contact);
+});
+
+module.exports = {getContacts, createContact, getContact, putContact, deleteContact};
